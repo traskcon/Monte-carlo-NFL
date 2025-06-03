6cb440d1c39b9f28a77caba58bbe5acc1f6eea7b
Connor Trask <traskcon@msu.edu>
2025-06-02 21:46:39 -0400

    Added ID, special teams data

--- sim_functions.R
+++ sim_functions.R
@@ -8,11 +8,14 @@ monte_carlo_model <- function(team, games, week_num, ...) {
   list(teams = teams, games = games)
 }
 
-games <- load_pbp(2024) |>
+rushes <- load_pbp(2024) |>
   filter(play_type == "run") |>
   mutate(pos_team = ifelse(posteam_type == "home", home_team, away_team)) |>
   mutate(def_team = ifelse(posteam_type == "home", away_team, home_team)) |>
   select(rusher_player_name, rusher_player_id, pos_team, def_team, yards_gained)
 
+current_rosters <- load_rosters(2025) |>
+  select(full_name, gsis_id)
+
 ggplot(games, aes(x=yards_gained)) + geom_histogram()
-write.csv(games, "c:/users/geniu/Documents/Monte-carlo-NFL/2024_rushes.csv")
+write.csv(rushes, "c:/users/geniu/Documents/Monte-carlo-NFL/2024_rushes.csv")
