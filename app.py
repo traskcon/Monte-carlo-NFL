6ce7be16aa12fe890942dc29b3327f516e1494cd
Connor Trask <ctrask@ida.org>
2025-06-11 12:59:29 -0400

    Changed from Dash to Shiny

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from dash import Dash, html
+import plotly.express as px
+from shiny.express import input, ui
+from shinywidgets import render_plotly
 import pandas as pd
 
-app = Dash()
+def load_game(home, away):
+    game_str = "./results/" + home + "v" + away + "stats.csv"
+    return pd.read_csv(game_str, header=[0,1], index_col=0)
 
-app.layout = [html.Div(children="Hello World")]
+test_df = load_game("PHI", "DAL")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+ui.page_opts(title="Monte Carlo Visualizations", fillable=True)
+
+with ui.layout_columns():
+
+    @render_plotly
+    def stat_hist():
+        return px.histogram(test_df["pass_yards"], x="Jalen Hurts")
