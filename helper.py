0edc91274c8350c67abe1fd3479487582889e67b
Connor Trask <connorptrask@gmail.com>
2025-08-05 10:40:12 -0400

    Moved time test to helper

--- helper.py
+++ helper.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from time import time
+from monte_carlo import Monte_Carlo_Sim
 
 def reshape_team_stats(team:str) -> pd.DataFrame:
     roster_df = pd.read_csv("./data/teams.csv", index_col=0)
@@ -23,3 +25,24 @@ def get_player_stats(team:str, matchup:str) -> pd.DataFrame:
         team_stats[stat] = team_stats["Player"].apply(lambda x: 
                                             np.mean(stat_df[stat].get(x,0)))
     return team_stats
+
+def time_test(sim:Monte_Carlo_Sim, type:str, n:int, cpu:int, verbose=False,
+              inlcude_startup=False):
+    # Currently just use PHI and DAL as default values for time_test
+    # Since we are only interested in timing and not results
+    home, away = "PHI", "DAL"
+    if inlcude_startup:
+        t1 = time()
+        sim = Monte_Carlo_Sim()
+    if type == "series":
+        t1 = time() if not inlcude_startup else t1
+        sim.run_simulations(home, away, n, verbose)
+        t2 = time()
+        return t2-t1
+    elif type == "parallel":
+        t1 = time() if not inlcude_startup else t1
+        sim.parallel_sim(home, away, n, cpu, verbose)
+        t2 = time()
+        return t2-t1
+    else:
+        print("Incorrect type argument. Please select 'series' or 'parallel'")
